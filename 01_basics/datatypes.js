397c592538ede41b01659bcaf13c2078f0db19a2
Biswoijt Biswal <115648972+Kargil99@users.noreply.github.com>
2024-06-10 00:43:19 +0000

    Datatypes in JS

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+/*
+
+Datatypes generally we use:
+
+1.number
+2.bigint
+3.string
+4.boolean
+5.null
+6.undefined
+7.symbol
+
+*/
+let name = "Kargil"
+let rollNumber = 202
+let Class = "XII"
+let isAgoodBoy = true
+
+console.table([typeof"name", rollNumber, Class, typeof(isAgoodBoy), typeof null, typeof undefined])
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("Biswojit")
+let accountId = 12345
+const accountName = "biswojit@gulgul.com"
+var acPW = "12345"
+accountCity = "BBSR"
+let accountState
+
+// accountName = "jaja" not allowed
+
+console.log(accountName);
+console.table([accountId, accountName, acPW, accountCity, accountState])
+/*
+console.table prints all the variables in a tabular form
+prefer to use let and const
+*/
